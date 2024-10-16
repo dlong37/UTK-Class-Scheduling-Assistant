4fb3febc51026933af68fdda9321312a8dc45846
tdehoff1 <tdehoff@vols.utk.edu>
2024-10-16 18:00:56 -0400

    Merged questionnaire updates

--- backend/views.py
+++ backend/views.py
@@ -83,10 +83,27 @@ def index():
 @login_required
 def submit():
     core_class = request.form.get('core-class')
+    sequence_classes = [
+        request.form.get('sequence-1'),
+        request.form.get('sequence-2'),
+        request.form.get('sequence-3'),
+        request.form.get('sequence-4'),
+        request.form.get('sequence-5'),
+    ]
     elective_credits = request.form.getlist('elective-credit')
+    credit_hours = request.form.get('credit-hours')
+    mwf_start_time = request.form.get('MWF-start-time')
+    tt_start_time = request.form.get('TT-start-time')
 
     # Prepare data for CSV
-    data = [core_class, ','.join(elective_credits)]
+    data = [
+        core_class,
+        *sequence_classes,
+        ','.join(elective_credits),
+        credit_hours,
+        mwf_start_time,
+        tt_start_time
+    ]
 
     # Write to CSV file
     with open('data.csv', mode='a', newline='') as file:
