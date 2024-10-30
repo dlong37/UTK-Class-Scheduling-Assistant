835d0eeee1532143878f91125eb40910cea76f59
tdehoff1 <tdehoff@vols.utk.edu>
2024-10-30 12:55:56 -0400

    Added port for deployment #21

--- backend/__init__.py
+++ backend/__init__.py
@@ -2,8 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_migrate import Migrate
-
-from os import path
+import os
 
 db = SQLAlchemy()
 DB_NAME = "database.db"
@@ -12,6 +11,9 @@ CLASSES_DB_NAME = "classes.db"
 # Initialize web app
 def create_app():
     app = Flask(__name__)
+    
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
     # Used for password hashing
     app.config['SECRET_KEY'] = 'bdqjfbkwbfjknjkwn'
 
